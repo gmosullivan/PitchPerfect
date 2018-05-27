14a98783ebb6aecb8bc02e4783a7c14830de8f57
garethosullivan <gareth.osullivan@icloud.com>
2018-05-27 13:59:21 +0100

    Added Buttons and labels for the RecordSoundsViewController and connected them to outlets and actions

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func recordSound() {
+        //Starts the recording
+    }
+    
+    @IBAction func stopRecording() {
+        //Stops the recording
     }
 
 }
